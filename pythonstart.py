a9caa1bdbe707e9e1b59d9911e02993bbee06664
MHassanEjaz <hassanejaz2216@gmail.com>
2025-04-10 19:32:25 +0500

    Initial commit

---
+++ pythonstart.py
@@ -0,0 +1,18 @@
+# print("Hello this is our first python program")
+# name = "Harry"
+# Age = 34
+# Price = 89.0
+
+# print(type(name))
+# print(type(Age))
+# print(type(Price))
+# from functools import reduce
+# def multiply(x):
+#     return x*2
+
+# numbers = [1,2,3,4,5]
+# product = reduce(multiply, numbers)
+# print(product)
+
+
+# 
