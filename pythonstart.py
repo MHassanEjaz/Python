5736ba26aef150b8d83c9a8ce47978fc306b1515
MHassanEjaz <hassanejaz2216@gmail.com>
2025-04-11 21:27:35 +0500

    Updated pythonstart.py

--- pythonstart.py
+++ pythonstart.py
@@ -2,3 +2,60 @@
 # name = "Harry"
 # Age = 34
 # Price = 89.0
+
+# Arithmetic, Relational, Assignment or Logical operator
+# a =10
+# b=4
+# sum = a + b
+# diff = a - b
+# div = a / b
+# mul = a * b
+# print(sum)
+# print(diff)
+# print(div)
+# print(mul)
+# print(a==b)
+# print(a!=b)
+# print(a<b)
+# print(a>b)
+# print(a<=b)
+# print(a>=b)
+
+# num = 10
+# num += 10
+# print("Num: ", num)
+
+# a=4
+# b=5
+# var1=True
+# var2=False
+# print("AND Operator: ", var1 and var2)
+# print("OR Operator: ", var1 or var2)
+# print("NOT Operator: ", var1 not var2)
+
+#type conversin
+# a =int("2")
+# b=9.0
+# print(a+b)
+
+# Practice with input
+# a = int(input("First Number= "))
+# b = int(input("Second Number= "))
+# print(a+b)
+
+# print area of a square
+# length = int(input("Enter Length= "))
+# width = int(input("Enter Width= "))
+# print("Area= ", length * width)
+
+
+# print average of two float numbers
+# a = int(input("Enter a= "))
+# b = int(input("Enter b= "))
+# print("Area= ", (a+b)/2)
+
+
+# print greater of two  numbers
+# a = int(input("Enter a= "))
+# b = int(input("Enter b= "))
+# print(a>=b)
