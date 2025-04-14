462a64a0adc5dbb7d7fcfaa3a1eb22db8980f3e1
MHassanEjaz <hassanejaz2216@gmail.com>
2025-04-14 20:50:25 +0500

    update

--- pythonstart.py
+++ pythonstart.py
@@ -251,14 +251,33 @@ lists are mutable means to be changed'''
 
 
 # Dictionaries and Set
-# Dictionaries to store data values in key:value pairs
-info = {
-    "Key" : "Value",
+# Dictionaries to store data values in key:value pairs and it is mutable
+# info = {
+#     "Key" : "Value",
+#     "Name" : "John",
+#     "Learning" : "Coding",
+#     "Age" : 35,
+#     "Subjects" : ["Python", "C++", "Java"],
+#     "Topics" : ("Dict", "Set")
+# }
+# info["Name"] = "Doe"
+# print(info)
+
+
+# Nested Dictionary
+student = {
     "Name" : "John",
-    "Learning" : "Coding",
-    "Age" : 35,
-    "Subjects" : ["Python", "C++", "Java"],
-    "Topics" : ("Dict", "Set")
-}
-print(info)
-  
+    "Subjects" : {
+        "Maths" : 45,
+        "Physice" : 43,
+        "Chemistry" : 76
+    }
+}  
+# print(student["Subjects"]["Chemistry"])
+# print(student.keys()) # return all keys
+# print(student.values()) # return values
+# print(student.items()) # return pairs
+# print(student.get("Name"))
+newdict = {"city" : "delhi"}
+student.update(newdict) # inserts the specified items to the dictionary
+print(student)
