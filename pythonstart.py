014bf2fe1caa4137b984c1ec53c1682de770376b
MHassanEjaz <hassanejaz2216@gmail.com>
2025-04-22 20:41:35 +0500

    update

--- pythonstart.py
+++ pythonstart.py
@@ -451,5 +451,23 @@ lists are mutable means to be changed'''
 #     idx+=1  
 
 # Range() function returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and stops before a specified number
+# seq=range(5)
+# for i in seq:
+#     print(i)
 
-  
+# for i in range(5): #range(stop)
+#     print(i)
+
+# for i in range(2, 5): #range(start, stop)
+#     print(i)
+
+# for i in range(2, 5, 2): #range(start, stop, step means increase by 2)
+#     print(i)
+
+# even numbers
+# for i in range(2, 10, 2):
+#    print(i)
+   
+# odd numbers
+# for i in range(1, 10, 2):
+#     print(i)   
