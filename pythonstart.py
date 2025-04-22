4a564c7f971426bac33a0fb1a06a76af96540f37
MHassanEjaz <hassanejaz2216@gmail.com>
2025-04-22 21:13:08 +0500

    update

--- pythonstart.py
+++ pythonstart.py
@@ -513,8 +513,8 @@ lists are mutable means to be changed'''
 #     i+=1
 # print("Total Factorail: ", fact)    
 
-n=5
-fact=1
-for i in range(1, n+1):
-    fact*=i
-print("Factorial is: ", fact)    
+# n=5
+# fact=1
+# for i in range(1, n+1):
+#     fact*=i
+# print("Factorial is: ", fact)    
