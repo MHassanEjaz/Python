55b3b1cae61fff6cad16926280d9396982f0ec01
MHassanEjaz <hassanejaz2216@gmail.com>
2025-07-22 22:33:35 +0500

    Save local changes before merge

---
+++ pythonstart.py
@@ -0,0 +1,13 @@
+# project 1 greetings
+  # name = input("Enter Your Name: ")
+  # print(f"Hello {name}. ")
+  # print("Welcome to the world of python")
+
+  # project 1 Band/Group name generator
+print("Welcome to the Band/Group Name Generator")
+color = input("What is your favorite color?")
+print(color)
+animal = input("What is your favorite animal?\n")
+print(animal)
+print(f"Your Band/Group name could be {color} {animal}. ")
+  
