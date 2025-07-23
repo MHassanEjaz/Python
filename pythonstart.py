3b903752e4f35d12c2c5ebe9df694309ea5be4bd
MHassanEjaz <hassanejaz2216@gmail.com>
2025-07-23 16:21:10 +0500

    Updated: your description

--- pythonstart.py
+++ pythonstart.py
@@ -741,14 +741,16 @@ lists are mutable means to be changed'''
 #     print(count)  
 
 # project 1 greetings
-  # name = input("Enter Your Name: ")
-  # print(f"Hello {name}. ")
-  # print("Welcome to the world of python")
+# name = input("Enter Your Name: ")
+# print(f"Hello {name}. ")
+# print("Welcome to the world of python")
 
-  # project 1 Band/Group name generator
+# project 1 Band/Group name generator
 # print("Welcome to the Band/Group Name Generator")
 # color = input("What is your favorite color?")
 # print(color)
 # animal = input("What is your favorite animal?\n")
 # print(animal)
 # print(f"Your Band/Group name could be {color} {animal}. ")
+
+# project 3 Gross Pay
