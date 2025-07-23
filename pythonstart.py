1337e2a3ba5b6d4b1e8c4a9d37af5522f424860f
MHassanEjaz <hassanejaz2216@gmail.com>
2025-07-23 21:30:26 +0500

    Updated: short description of today's changes

--- pythonstart.py
+++ pythonstart.py
@@ -768,12 +768,12 @@ lists are mutable means to be changed'''
 #   print(fahrenheit)
 
 
-# # project 5 Trip Cost Calculator
-#    print("Welcome to the Trip Cost Calculator ")
-#    days = int(input("Enter number of days: "))
-#    cost = float(input("Enter cost per night: "))
-#    flightcost = float(input("Enter flight cost: "))
-#    carprice = float(input("If you need rental car enter 1 else 0: "))
-#    otherexpense = float(input("Enter other expenses: "))
-#    totalcost = round(days * cost + flightcost + days * carprice + otherexpense, 2)
-#    print(f"Your total cost is {totalcost} ")
+# project 5 Trip Cost Calculator
+   print("Welcome to the Trip Cost Calculator ")
+   days = int(input("Enter number of days: "))
+   cost = float(input("Enter cost per night: "))
+   flightcost = float(input("Enter flight cost: "))
+   carprice = float(input("If you need rental car enter 1 else 0: "))
+   otherexpense = float(input("Enter other expenses: "))
+   totalcost = round(days * cost + flightcost + days * carprice + otherexpense, 2)
+   print(f"Your total cost is {totalcost} ")
