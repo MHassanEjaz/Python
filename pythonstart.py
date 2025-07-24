544cb0943bf03b2400dc00936d32b95fba48f5a5
MHassanEjaz <hassanejaz2216@gmail.com>
2025-07-24 23:01:17 +0500

    Quick update

--- pythonstart.py
+++ pythonstart.py
@@ -1049,21 +1049,21 @@ lists are mutable means to be changed'''
 
 
 # Project 10 Score Checker
-try:
-    score = float(input("Enter your Score: "))
-except ValueError:
-    print("Bad Score")
-    quit()    
-if score  >= 0.0 and score <=1.0:
-    if score >= 0.9:
-        print("A")
-    elif score >= 0.8:
-        print("B")
-    elif score >= 0.7:
-        print("C")
-    elif score >= 0.6:
-        print("D")
-    else:
-        print("F")                
-else:
-    print("Bad Score. ")    
+# try:
+#     score = float(input("Enter your Score: "))
+# except ValueError:
+#     print("Bad Score")
+#     quit()    
+# if score  >= 0.0 and score <=1.0:
+#     if score >= 0.9:
+#         print("A")
+#     elif score >= 0.8:
+#         print("B")
+#     elif score >= 0.7:
+#         print("C")
+#     elif score >= 0.6:
+#         print("D")
+#     else:
+#         print("F")                
+# else:
+#     print("Bad Score. ")    
