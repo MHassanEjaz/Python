ec4ef80c8c5cbbf6cdf9efc03f60be7bd74e206d
MHassanEjaz <hassanejaz2216@gmail.com>
2025-07-28 21:44:31 +0500

    Quick update

--- pythonstart.py
+++ pythonstart.py
@@ -1408,18 +1408,34 @@ lists are mutable means to be changed'''
 
 
 # Sum of above average scores
-student_scores = [80, 60, 50, 65, 75, 55]
-def sum_score_above_average(p_student_scores):
-    sum_score = 0
-    number_of_students = 0
-    for score in student_scores:
-        sum_score += score
-        number_of_students += 1
-    average_score =  sum_score / number_of_students
-    sum_above_average = 0
-    for score in p_student_scores:
-        if score > average_score:
-            sum_above_average += score
-    return sum_above_average
-print(sum_score_above_average(student_scores))             
+# student_scores = [80, 60, 50, 65, 75, 55]
+# def sum_score_above_average(p_student_scores):
+#     sum_score = 0
+#     number_of_students = 0
+#     for score in student_scores:
+#         sum_score += score
+#         number_of_students += 1
+#     average_score =  sum_score / number_of_students
+#     sum_above_average = 0
+#     for score in p_student_scores:
+#         if score > average_score:
+#             sum_above_average += score
+#     return sum_above_average
+# print(sum_score_above_average(student_scores)) 
+
+
+
+
+# Loop over custom function
+# def password_controller(password):
+#     if len(password) > 8:
+#         return True
+#     else:
+#         return False
+# # result = password_controller("cust")    
+# # print(result)
+# passwor_list = ["qwer", "123456", "098765432", "abcdefgh"]
+# for password in passwor_list:
+#     result = password_controller(password)
+#     print(password, result)            
          
