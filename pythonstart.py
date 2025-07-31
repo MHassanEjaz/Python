98c568a3b27c3acd3f3c62d79e05c068d63a0aac
MHassanEjaz <hassanejaz2216@gmail.com>
2025-07-31 20:31:40 +0500

    Quick update

--- pythonstart.py
+++ pythonstart.py
@@ -1717,5 +1717,14 @@ lists are mutable means to be changed'''
 # letter = fruit[1]
 # print(a)
 
-newstring = "Hello Python"
-print(newstring[0:7])
+# newstring = "Hello Python"
+# print(newstring[0:7])
+# print(newstring[:])
+
+
+# Sum of Digits of 2 digit number
+def sum_of_two_digits():
+    two_digit_number = input("Enter two digit number: ")
+    sum_of_digits = int(two_digit_number[0]) + int(two_digit_number[1])
+    return sum_of_digits
+print(sum_of_two_digits())
