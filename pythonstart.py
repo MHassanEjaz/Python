3af5e45302a4f49844a99a6d1b356cf92491577f
MHassanEjaz <hassanejaz2216@gmail.com>
2025-07-31 21:44:06 +0500

    Quick update

--- pythonstart.py
+++ pythonstart.py
@@ -1786,4 +1786,9 @@ lists are mutable means to be changed'''
 
 # coding exercise first and last two characters
 def first_last_characters(word):
-    
+    if len(word) < 2:
+        return ''
+    first_two_chars = word[0:2]
+    last_two_chars = word[-2:]
+    return first_two_chars + last_two_chars
+print(first_last_characters("appmillers"))    
