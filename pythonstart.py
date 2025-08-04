d27b41b4842321669ea47cd7279c437399cf6d4f
MHassanEjaz <hassanejaz2216@gmail.com>
2025-08-04 21:11:06 +0500

    Quick update

--- pythonstart.py
+++ pythonstart.py
@@ -1886,10 +1886,14 @@ lists are mutable means to be changed'''
 
 
 # coding exercise square of items
-def square_list(p_list):
-    for index in range(len(p_list)):
-        p_list[index] = p_list[index] * p_list[index]
-    return p_list
+# def square_list(p_list):
+#     for index in range(len(p_list)):
+#         p_list[index] = p_list[index] * p_list[index]
+#     return p_list
 
-custom_list = [1,2,3,4,5,6,7,8,9,10]
-print(square_list(custom_list))    
+# custom_list = [1,2,3,4,5,6,7,8,9,10]
+# print(square_list(custom_list))    
+
+
+
+# index out of range
