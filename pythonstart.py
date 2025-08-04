7f83b259d622017a366b1ec43178d0bc384f4c29
MHassanEjaz <hassanejaz2216@gmail.com>
2025-08-04 20:29:24 +0500

    Quick update

--- pythonstart.py
+++ pythonstart.py
@@ -1874,11 +1874,11 @@ lists are mutable means to be changed'''
 
 
 
-project 20 String Formatting
-names = ['John', 'Edy', 'Jane', 'Kane']
-scores = [90, 95, 80, 75]
-print('{0:<10} {1:<5}'.format("Name", "Score"))
-for index in range(len(names)):
-    name = names[index]
-    score = scores[index]
-    print('{0:<10} {1:<5}'.format(name, score))
+# project 20 String Formatting
+# names = ['John', 'Edy', 'Jane', 'Kane']
+# scores = [90, 95, 80, 75]
+# print('{0:<10} {1:<5}'.format("Name", "Score"))
+# for index in range(len(names)):
+#     name = names[index]
+#     score = scores[index]
+#     print('{0:<10} {1:<5}'.format(name, score))
