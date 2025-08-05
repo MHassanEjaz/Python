c560ff2cdeff010d5dc45907d74a26f4a2539bca
MHassanEjaz <hassanejaz2216@gmail.com>
2025-08-05 20:27:48 +0500

    Quick update

--- pythonstart.py
+++ pythonstart.py
@@ -2024,3 +2024,6 @@ sample_list = [21,55,18,33,24,22,68,35,79]
 length = len(sample_list)
 chunk_size = int(length/3)
 start = 0
+end = chunk_size
+for i in range(1,4):
+    list_chunk = sample_list[start:end]
