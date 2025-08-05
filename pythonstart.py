f78700a60e6dc5e06349114a11659ed0412348ec
MHassanEjaz <hassanejaz2216@gmail.com>
2025-08-05 20:22:34 +0500

    Quick update

--- pythonstart.py
+++ pythonstart.py
@@ -2011,3 +2011,8 @@ lists are mutable means to be changed'''
 
 
 # coding exercise remove and add value
+custom_list = [10,44,57,99,11,33,84]
+element = custom_list.pop(4)
+custom_list.insert(2, element)
+custom_list.append(element)
+print(custom_list)
