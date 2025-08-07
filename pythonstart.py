e14aac07fa7c54fb16565375bf2c0a80aa70374a
MHassanEjaz <hassanejaz2216@gmail.com>
2025-08-07 22:40:24 +0500

    Quick update

--- pythonstart.py
+++ pythonstart.py
@@ -2333,87 +2333,108 @@ lists are mutable means to be changed'''
 # Caesar Cipher Program
 
 # Alphabet list
+# alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+#             'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+# # Encryption function
+# def encrypt(p_message, p_shift_number):
+#     cipher_message = ""
+#     for char in p_message:
+#         if char in alphabet:
+#             position = alphabet.index(char)
+#             new_position = (position + p_shift_number) % 26
+#             cipher_message += alphabet[new_position]
+#         else:
+#             cipher_message += char   
+#     return f"The encoded message is: {cipher_message}"
+
+# # Decryption function
+# def decrypt(p_message, p_shift_number):
+#     message = ""
+#     for char in p_message:
+#         if char in alphabet:
+#             position = alphabet.index(char)
+#             old_position = (position - p_shift_number) % 26
+#             message += alphabet[old_position]
+#         else:
+#             message += char    
+#     return f"The decoded message is: {message}"
+
+# # Main loop
+# end_program = False
+# while not end_program:
+#     enc_dec = input("Type 'E' to encrypt, type 'D' to decrypt:\n").upper()
+#     message = input("Enter your message:\n").upper()
+#     shift_number = int(input("Enter the shift number:\n"))
+
+#     if enc_dec == "E":
+#         encrypted_message = encrypt(message, shift_number)
+#         print(encrypted_message)
+#     elif enc_dec == "D":
+#         decrypted_message = decrypt(message, shift_number)
+#         print(decrypted_message)
+#     else:
+#         print("Invalid option. Please type 'E' or 'D'.")
+
+#     restart = input("Type 'Y' if you want to continue, otherwise type 'N':\n").upper()
+#     if restart == "N":
+#         end_program = True
+#         print("See you next time!")
+
+
+
+
+# Caesar Cipher Program
+
+# Alphabet list
 alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
             'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-
+# Recalculate position for encryption or decryption
 def refactor_position(p_position, p_cipher_type):
     if p_cipher_type == 'E':
         while p_position > 25:
-            p_position = p_position - 26
+            p_position -= 26
         return p_position
     else:
         while p_position < 0:
-            p_position = p_position + 26
+            p_position += 26
         return p_position
-    
-def caesar_chiper(p_initial_text, p_shift_amount, p_cipher_type):
-  final_text = ""
-  if p_cipher_type == "D":
-    p_shift_amount *= -1
-  for char in p_initial_text:
-    if char in alphabet:
-      position = alphabet.index(char)
-      new_position = position + p_shift_amount
-      new_position = refactor_position(new_position, p_cipher_type)
-      final_text += alphabet[new_position]
-    else:
-      final_text += char
-  print(f"Here's the {'decode' if p_cipher_type=='D' else 'encode'}d result: {final_text}")    
 
+# Core Caesar cipher function
+def caesar_cipher(p_initial_text, p_shift_amount, p_cipher_type):
+    final_text = ""
+    if p_cipher_type == "D":
+        p_shift_amount *= -1
 
-
-# Encryption function
-def encrypt(p_message, p_shift_number):
-    cipher_message = ""
-    for char in p_message:
+    for char in p_initial_text:
         if char in alphabet:
             position = alphabet.index(char)
-            new_position = (position + p_shift_number) % 26
-            cipher_message += alphabet[new_position]
+            new_position = position + p_shift_amount
+            new_position = refactor_position(new_position, p_cipher_type)
+            final_text += alphabet[new_position]
         else:
-            cipher_message += char   
-    return f"The encoded message is: {cipher_message}"
+            final_text += char
 
-# Decryption function
-def decrypt(p_message, p_shift_number):
-    message = ""
-    for char in p_message:
-        if char in alphabet:
-            position = alphabet.index(char)
-            old_position = (position - p_shift_number) % 26
-            message += alphabet[old_position]
-        else:
-            message += char    
-    return f"The decoded message is: {message}"
+    action = "decoded" if p_cipher_type == "D" else "encoded"
+    print(f"Here's the {action} result: {final_text}")
 
-# Import and display ASCII logo (if available)
-try:
-    from logo import logo
-    print(logo)
-except ImportError:
-    print("Welcome to Caesar Cipher!")
 
-# Main loop
+
+# Main program loop
 end_program = False
 while not end_program:
     enc_dec = input("Type 'E' to encrypt, type 'D' to decrypt:\n").upper()
-    message = input("Enter your message:\n").upper()
-    shift_number = int(input("Enter the shift number:\n"))
+    text = input("Enter your message:\n").upper()
+    shift = int(input("Enter the shift number:\n"))
 
-    if enc_dec == "E":
-        encrypted_message = encrypt(message, shift_number)
-        print(encrypted_message)
-    elif enc_dec == "D":
-        decrypted_message = decrypt(message, shift_number)
-        print(decrypted_message)
-    else:
-        print("Invalid option. Please type 'E' or 'D'.")
+    caesar_cipher(p_initial_text=text, p_shift_amount=shift, p_cipher_type=enc_dec)
 
-    restart = input("Type 'Y' if you want to continue, otherwise type 'N':\n").upper()
+    restart = input("Type 'Y' if you want to go again. Otherwise type 'N':\n").upper()
     if restart == "N":
         end_program = True
         print("See you next time!")
+
   
          
       
