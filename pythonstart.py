676d8bc97d616359b440ff5a3c872225a28a7ef6
MHassanEjaz <hassanejaz2216@gmail.com>
2025-09-01 22:31:22 +0500

    Quick update

--- pythonstart.py
+++ pythonstart.py
@@ -2940,4 +2940,16 @@ mylist = [1,2,3,4,5,[1,2],[4,5]]
 
 
 
-# 
+# keys in dictionary
+custom_dict = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+}
+devices_list = {"phone", "tablet", "computer", "tv"}
+new_dict = {}.fromkeys(devices_list, 0)
+print(new_dict)
