1ead796a61512958dac33041c0abcad017fb7f58
MHassanEjaz <hassanejaz2216@gmail.com>
2025-09-02 19:30:40 +0500

    Quick update

--- pythonstart.py
+++ pythonstart.py
@@ -2961,4 +2961,6 @@ mylist = [1,2,3,4,5,[1,2],[4,5]]
 # Coding exercise solution to group value type
 custom_list = [10,"one", "two", "ten", 20,30,"five", 40,"nine", 50]
 def group_types(p_list):
-    output_dict = dict.fromkeys()
+    output_dict = dict.fromkeys(p_list)
+    print(output_dict)
+group_types(custom_list)
