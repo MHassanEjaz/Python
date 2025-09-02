285d3aa51262ceae4936aa8c0c0224e777cffc8f
MHassanEjaz <hassanejaz2216@gmail.com>
2025-09-02 19:35:33 +0500

    Quick update

--- pythonstart.py
+++ pythonstart.py
@@ -2959,13 +2959,28 @@ mylist = [1,2,3,4,5,[1,2],[4,5]]
 
 
 # Coding exercise solution to group value type
-custom_list = [10,"one", "two", "ten", 20,30,"five", 40,"nine", 50]
-def group_types(p_list):
-    output_dict = dict.fromkeys(p_list)
-    for key in output_dict:
-        if isinstance(key, int):
-            output_dict[key] = "Integer"
-        else:
-            output_dict[key] = "String"
-    return output_dict
-print(group_types(custom_list))
+# custom_list = [10,"one", "two", "ten", 20,30,"five", 40,"nine", 50]
+# def group_types(p_list):
+#     output_dict = dict.fromkeys(p_list)
+#     for key in output_dict:
+#         if isinstance(key, int):
+#             output_dict[key] = "Integer"
+#         else:
+#             output_dict[key] = "String"
+#     return output_dict
+# print(group_types(custom_list))
+
+
+
+# Dictionary item() method
+custom_dict = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+}
+
+
