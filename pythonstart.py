f2f7588aed8b0f8cb926f06a5b58887009a83ef7
MHassanEjaz <hassanejaz2216@gmail.com>
2025-09-02 21:14:30 +0500

    Quick update

--- pythonstart.py
+++ pythonstart.py
@@ -3123,6 +3123,10 @@ lists are mutable means to be changed'''
 
 # coding exercise merge two dictionary
 dict1 = {"One":2, "Two":2, "Three":3}
-dict1 = {"Three":3, "Four":4, "Five":5}
-def merge_dict(dict1, dixt2):
+dict2 = {"Three":3, "Four":4, "Five":5}
+def merge_dict(p_dict1, p_dict2):
+    dict3 = p_dict1.copy()
+    dict3.update(p_dict2)
+    return dict3
+print(merge_dict(dict1, dict2))
     
