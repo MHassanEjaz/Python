23445ba654f95e25abde54c0ce8b204005fea69a
MHassanEjaz <hassanejaz2216@gmail.com>
2025-09-02 20:22:24 +0500

    Quick update

--- pythonstart.py
+++ pythonstart.py
@@ -3085,7 +3085,7 @@ custom_dict = {
     "city": None
 }
 def remove_empty_items(p_dict):
-    for key, value in p_dict.item():
+    for key, value in list(p_dict.items()):
         if value is None:
             p_dict.pop(key)
     return p_dict
