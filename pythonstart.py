d19f99783ad625c00bb8a96aa38d6831241011b0
MHassanEjaz <hassanejaz2216@gmail.com>
2025-09-02 19:33:20 +0500

    Quick update

--- pythonstart.py
+++ pythonstart.py
@@ -2962,5 +2962,10 @@ mylist = [1,2,3,4,5,[1,2],[4,5]]
 custom_list = [10,"one", "two", "ten", 20,30,"five", 40,"nine", 50]
 def group_types(p_list):
     output_dict = dict.fromkeys(p_list)
-    print(output_dict)
-group_types(custom_list)
+    for key in output_dict:
+        if isinstance(key, int):
+            output_dict[key] = "Inteher"
+        else:
+            output_dict[key] = "String"
+    return output_dict
+print(group_types(custom_list))
