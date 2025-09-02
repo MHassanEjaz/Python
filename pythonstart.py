1c743994fe56e37394567df4f2808e81fdf68005
MHassanEjaz <hassanejaz2216@gmail.com>
2025-09-02 20:24:14 +0500

    Quick update

--- pythonstart.py
+++ pythonstart.py
@@ -3079,14 +3079,19 @@ lists are mutable means to be changed'''
 
 
 # coding exercise remove empty items
-custom_dict = {
-    "name": "Elshad",
-    "age": 28,
-    "city": None
-}
-def remove_empty_items(p_dict):
-    for key, value in list(p_dict.items()):
-        if value is None:
-            p_dict.pop(key)
-    return p_dict
-print(remove_empty_items(custom_dict))
+# custom_dict = {
+#     "name": "Elshad",
+#     "age": 28,
+#     "city": None
+# }
+# def remove_empty_items(p_dict):
+#     for key, value in list(p_dict.items()):
+#         if value is None:
+#             p_dict.pop(key)
+#     return p_dict
+# print(remove_empty_items(custom_dict))
+
+
+
+
+# Reference to dictionary copy() method
