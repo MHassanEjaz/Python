dc62285d2b793a061e2746b325ae685fc8a809ec
MHassanEjaz <hassanejaz2216@gmail.com>
2025-09-04 20:34:28 +0500

    Quick update

--- pythonstart.py
+++ pythonstart.py
@@ -3291,7 +3291,11 @@ print("There are total of 6 questions, you can skip a question anytime by typing
 def check_answer(question_num, answer, attempts, player):
     correct_answer = quiz[question_num]["answer"]
     if correct_answer.lower() == answer.lower():
-        print(f"Correct Answer! \n")
+        print(f"Correct Answer! \n{player}'s score is {player_score[player] + 1} ")
+        return True
+    else:
+        
+        
 quiz = {
     1 : {
         "question" : "A mapping from a set of keys to their corresponding values",
