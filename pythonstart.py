c07db61a974570e00693fbad198b61edae82aec4
MHassanEjaz <hassanejaz2216@gmail.com>
2026-02-12 15:21:20 +0500

    Quick update

--- pythonstart.py
+++ pythonstart.py
@@ -3512,5 +3512,8 @@ lists are mutable means to be changed'''
 
 a= {1,2,3,4}
 b={5,6,7,8}
-print(a | b)
-print(a.union(b))
+# print(a | b)
+# print(a.union(b))
+set1 = set(a) & set(b)
+print(set1)
+print(sorted(a))
