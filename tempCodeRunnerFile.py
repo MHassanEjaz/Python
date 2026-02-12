8b489ec431649a04e4f8eadb949a0caf3af2a041
MHassanEjaz <hassanejaz2216@gmail.com>
2026-02-12 15:24:20 +0500

    Quick update

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,2 @@
-def concatenate(p_list1, p_list2):
-    list3 = []
-    for item1 in p_list1:
-        for item2 in p_list2:
-            list3.append(item1 + item2)
-    return list3
-list1= ["Hello ", "take "]
-list2= ["Dear ", "Sir "]
-concatenate(list1, list2)  
+print(a | b)
+# print(a.union(b))
