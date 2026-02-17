fdb7bfcd4da2492c7e7ae32754863ee571c5b40e
MHassanEjaz <hassanejaz2216@gmail.com>
2026-02-17 19:35:06 +0500

    Quick update

--- pythonstart.py
+++ pythonstart.py
@@ -3786,5 +3786,9 @@ import numpy as np
 # a = np.array([[1,2,3]])
 # print(a)
 
-a=np.array([[1,2,3], [4,5,6]])
+# a=np.array([[1,2,3], [4,5,6]])
+# print(a)
+
+
+a=np.array([[1,2,3], [4,5,6], [7,8,9]])
 print(a)
