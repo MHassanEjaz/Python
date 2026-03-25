5bcec748649cb7fda1e5eacddd54b9ba6ab115e2
MHassanEjaz <hassanejaz2216@gmail.com>
2026-03-25 19:28:55 +0500

    Quick update

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,26 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+print("Hello this is our first python program")
+name = "Harry"
+Age = 34
+Price = 89.0
 
-Data = {'Year': [1920,1930,1940,1950,1960,1970,1980,1990,2000, 2010, 2020],
-       'Exchange Rate': [65, 69, 71, 64, 62, 59, 72, 71, 75, 78, 81]}
-df = pd.DataFrame(Data, columns = ['Year', 'Exchange Rate'])
-df.plot(x='Year', y= 'Exchange Rate', kind= 'bar')
-print(df.show)
+Arithmetic, Relational, Assignment or Logical operator
+a =10
+b=4
+sum = a + b
+diff = a - b
+div = a / b
+mul = a * b
+print(sum)
+print(diff)
+print(div)
+print(mul)
+print(a==b)
+print(a!=b)
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+
+num = 10
+num += 10
+print("Num: ", num)
