fc59ff4c57fea56ae6f71549bdbda6da4e17e0db
MHassanEjaz <hassanejaz2216@gmail.com>
2026-04-23 21:19:49 +0500

    Quick update

--- pythonstart.py
+++ pythonstart.py
@@ -1,7 +1,7 @@
-# print("Hello this is our first python program")
-# name = "Harry"
-# Age = 34
-# Price = 89.0
+print("Hello this is our first python program")
+name = "Harry"
+Age = 34
+Price = 89.0
 
 # Arithmetic, Relational, Assignment or Logical operator
 # a =10
