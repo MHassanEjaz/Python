48d66262efe65759becaf662e46085b5a755b86a
MHassanEjaz <hassanejaz2216@gmail.com>
2026-05-06 21:30:05 +0500

    Quick update

--- pythonstart.py
+++ pythonstart.py
@@ -1,29 +1,29 @@
-print("Hello this is our first python program")
-name = "Harry"
-Age = 34
-Price = 89.0
+# print("Hello this is our first python program")
+# name = "Harry"
+# Age = 34
+# Price = 89.0
 
-Arithmetic, Relational, Assignment or Logical operator
-a =10
-b=4
-sum = a + b
-diff = a - b
-div = a / b
-mul = a * b
-print(sum)
-print(diff)
-print(div)
-print(mul)
-print(a==b)
-print(a!=b)
-print(a<b)
-print(a>b)
-print(a<=b)
-print(a>=b)
+# Arithmetic, Relational, Assignment or Logical operator
+# a =10
+# b=4
+# sum = a + b
+# diff = a - b
+# div = a / b
+# mul = a * b
+# print(sum)
+# print(diff)
+# print(div)
+# print(mul)
+# print(a==b)
+# print(a!=b)
+# print(a<b)
+# print(a>b)
+# print(a<=b)
+# print(a>=b)
 
-num = 10
-num += 10
-print("Num: ", num)
+# num = 10
+# num += 10
+# print("Num: ", num)
 
 # a=4
 # b=5
